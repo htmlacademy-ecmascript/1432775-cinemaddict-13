69dc09447f73fd3f0d9139ad94f9e0d4db201eb2
Ibarsuk <ivanbarsukov7@gmail.com>
2020-11-19 10:02:17 +0300

    3. Время разбивать камни

---
+++ src/view/film-card.js
@@ -0,0 +1,19 @@
+export const createFilmCard = () => {
+  return `<article class="film-card">
+  <h3 class="film-card__title">Title</h3>
+  <p class="film-card__rating">0</p>
+  <p class="film-card__info">
+    <span class="film-card__year">0000</span>
+    <span class="film-card__duration">0m</span>
+    <span class="film-card__genre">genre</span>
+  </p>
+  <img src="./images/posters/900x1366.png" alt="" class="film-card__poster">
+  <p class="film-card__description">description</p>
+  <a class="film-card__comments">0 comments</a>
+  <div class="film-card__controls">
+    <button class="film-card__controls-item button film-card__controls-item--add-to-watchlist film-card__controls-item--active" type="button">Add to watchlist</button>
+    <button class="film-card__controls-item button film-card__controls-item--mark-as-watched" type="button">Mark as watched</button>
+    <button class="film-card__controls-item button film-card__controls-item--favorite" type="button">Mark as favorite</button>
+  </div>
+</article>`;
+};
---
+++ src/view/loading.js
@@ -0,0 +1,7 @@
+export const createLoading = () => {
+  return `<section class="films">
+  <section class="films-list">
+    <h2 class="films-list__title">Loading...</h2>
+  </section>
+</section>`;
+};
