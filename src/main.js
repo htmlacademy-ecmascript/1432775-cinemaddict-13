ab3f83a0ec52ec140299c5be4f20ddfca852c489
Ibarsuk <ivanbarsukov7@gmail.com>
2020-11-29 14:49:11 +0300

    Задание 6

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ import UserMock from './mock/user';
 import {render} from './util.js';
 import {renderCard} from './render-card';
 
-const MOCK_FILMS = 15;
+const MOCK_FILMS = 1;
 const FILMS_CARDS_NUMBER = 5;
 const FILMS_STEP_LOAD = 5;
 const FILMS_TOP_RAITED_CARDS_NUMBER = 2;
@@ -27,55 +27,65 @@ const siteHeader = document.querySelector(`.header`);
 render(siteHeader, new UserIconView(user.avatar, user.raiting).getElement());
 
 const siteMain = document.querySelector(`.main`);
-const siteCatalog = new SiteCatalogView();
-const filmsContainer = siteCatalog.getElement();
 render(siteMain, new SiteMenuView(user).getElement(), `afterbegin`);
-render(siteMain, new SiteSortView().getElement());
-render(siteMain, filmsContainer);
 
-const filmsListContainer = filmsContainer.querySelector(`.films-list__container`);
-for (let i = 0; i < Math.min(FILMS_CARDS_NUMBER, films.length); i++) {
-  renderCard(filmsListContainer, films[i]);
-}
+const renderTheWholeCatalog = () => {
+  const siteCatalog = new SiteCatalogView(MOCK_FILMS < 1);
+  const filmsContainer = siteCatalog.getElement();
 
-if (films.length > Math.max(FILMS_STEP_LOAD, FILMS_CARDS_NUMBER)) {
-  let renderedFilms = FILMS_CARDS_NUMBER;
+  if (MOCK_FILMS < 1) {
+    render(siteMain, filmsContainer);
+    return;
+  }
 
-  const onShowMoreButtonClick = () => {
-    films.slice(renderedFilms, renderedFilms + FILMS_STEP_LOAD).forEach((film) => renderCard(filmsListContainer, film));
+  render(siteMain, new SiteSortView().getElement());
+  render(siteMain, filmsContainer);
 
-    renderedFilms += FILMS_STEP_LOAD;
+  const filmsListContainer = filmsContainer.querySelector(`.films-list__container`);
+  for (let i = 0; i < Math.min(FILMS_CARDS_NUMBER, films.length); i++) {
+    renderCard(filmsListContainer, films[i]);
+  }
 
-    if (renderedFilms >= films.length) {
-      showBoreButton.remove();
-      showBoreButton.removeEventListener(`click`, onShowMoreButtonClick);
-    }
-  };
+  if (films.length > Math.max(FILMS_STEP_LOAD, FILMS_CARDS_NUMBER)) {
+    let renderedFilms = FILMS_CARDS_NUMBER;
 
-  const filmsCatalog = filmsContainer.querySelector(`.films-list`);
-  render(filmsCatalog, new ShowMoreButtonView().getElement());
+    const onShowMoreButtonClick = () => {
+      films.slice(renderedFilms, renderedFilms + FILMS_STEP_LOAD).forEach((film) => renderCard(filmsListContainer, film));
 
-  const showBoreButton = filmsCatalog.querySelector(`.films-list__show-more`);
-  showBoreButton.addEventListener(`click`, onShowMoreButtonClick);
-}
+      renderedFilms += FILMS_STEP_LOAD;
 
-render(filmsContainer, new TopRaitedContainerView().getElement());
-render(filmsContainer, new MostCommentedContainerView().getElement());
+      if (renderedFilms >= films.length) {
+        showBoreButton.remove();
+        showBoreButton.removeEventListener(`click`, onShowMoreButtonClick);
+      }
+    };
 
-const topRaitedFilmsContainer = filmsContainer.querySelector(`.films-list--extra .films-list__container`);
-const filmsSortedByRaiting = films.slice().sort((previous, current) => {
-  return current.raiting - previous.raiting;
-});
-for (let i = 0; i < FILMS_TOP_RAITED_CARDS_NUMBER; i++) {
-  renderCard(topRaitedFilmsContainer, filmsSortedByRaiting[i]);
-}
+    const filmsCatalog = filmsContainer.querySelector(`.films-list`);
+    render(filmsCatalog, new ShowMoreButtonView().getElement());
 
-const mostCommentedFilmsContainer = filmsContainer.querySelector(`.films-list--commented .films-list__container`);
-for (let i = 0; i < FILMS_MOST_COMMENTED_CARDS_NUMBER; i++) {
-  renderCard(mostCommentedFilmsContainer, films.slice().sort((previous, current) => {
-    return current.comments.length - previous.comments.length;
-  })[i]);
-}
+    const showBoreButton = filmsCatalog.querySelector(`.films-list__show-more`);
+    showBoreButton.addEventListener(`click`, onShowMoreButtonClick);
+  }
+
+  render(filmsContainer, new TopRaitedContainerView().getElement());
+  render(filmsContainer, new MostCommentedContainerView().getElement());
+
+  const topRaitedFilmsContainer = filmsContainer.querySelector(`.films-list--extra .films-list__container`);
+  const filmsSortedByRaiting = films.slice().sort((previous, current) => {
+    return current.raiting - previous.raiting;
+  });
+  for (let i = 0; i < FILMS_TOP_RAITED_CARDS_NUMBER; i++) {
+    renderCard(topRaitedFilmsContainer, filmsSortedByRaiting[i]);
+  }
+
+  const mostCommentedFilmsContainer = filmsContainer.querySelector(`.films-list--commented .films-list__container`);
+  for (let i = 0; i < FILMS_MOST_COMMENTED_CARDS_NUMBER; i++) {
+    renderCard(mostCommentedFilmsContainer, films.slice().sort((previous, current) => {
+      return current.comments.length - previous.comments.length;
+    })[i]);
+  }
+};
+renderTheWholeCatalog();
 
 const siteFooter = document.querySelector(`.footer`);
 const footerStats = siteFooter.querySelector(`.footer__statistics`);
--- src/view/films-catalog.js
+++ src/view/films-catalog.js
@@ -1,20 +1,24 @@
 import {createElement} from '../util.js';
 
-const createFilmsCatalog = () => {
+const createFilmsCatalog = (isCatalogEmpty) => {
   return `<section class="films">
   <section class="films-list">
-    <h2 class="films-list__title visually-hidden">All movies. Upcoming</h2>
-    <div class="films-list__container"></div>
+    ${isCatalogEmpty ?
+    `<h2 class="films-list__title">There are no movies in our database</h2>` :
+
+    `<h2 class="films-list__title visually-hidden">All movies. Upcoming</h2>
+    <div class="films-list__container"></div>`}
   </section>`;
 };
 
 export default class SiteCatalog {
-  constructor() {
+  constructor(isEmpty) {
     this._element = null;
+    this._isEmpty = isEmpty;
   }
 
   getTemplate() {
-    return createFilmsCatalog();
+    return createFilmsCatalog(this._isEmpty);
   }
 
   getElement() {
